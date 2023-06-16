e1a9c65b20688e44ac7ad1bbf1713afa16f4ad52
PedroJuri <p3dr1n018@outlook.com>
2023-06-15 22:18:58 -0300

    Fazendo as alterações e subindo a calculadora

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #28374d;
+    background-color: #1FBAC0;
+    color: #28374d;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    
+    font-family: 'Roboto';
+    padding 0 10px;
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #61D3AB;
+        border: 1px solid #28374d;
+        padding: 0 10px;
+        text-align: right;
+        font-size: 24px;
+        color: #28374d;
+    }
+`
